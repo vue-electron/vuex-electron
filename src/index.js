168ba8e5a8efa86be20da2bc4c21d0ee27fce594
Douglass <doug@douglassllc.com>
2024-03-27 11:38:12 +0100

    chore(deps): upgrade all deps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import createPersistedState from "./persisted-state"
 import createSharedMutations from "./shared-mutations"
 
+import { version } from '../package.json'
+
+console.log(`Loaded vuex-electron v${version}`)
+
 export { createPersistedState, createSharedMutations }
